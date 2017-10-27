337c09f28d94139de6614e1cf6e9b507eb3550bc
Sabeeh <sabeeh.shah@hotmail.com>
2017-10-26 22:06:43 -0400

    c

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -29,14 +29,14 @@ user:User;
 
   onSubmit(){
 
-    // this.user.username = this.email;
-    // this.user.createdDate = new Date().toLocaleDateString();
-    // this.user.createdTime = new Date().toLocaleTimeString();
-
-    // this.userService.newUser(this.user);
-
     this.authService.register(this.email,this.password)
     .then((res) => {
+      this.user.username = this.email;
+      this.user.createdDate = new Date().toLocaleDateString();
+      this.user.createdTime = new Date().toLocaleTimeString();
+  
+      this.userService.newUser(this.user);
+  
       this.flashMessagesService.show('New user registered.', { cssClass: 'alert-success', timeout:4000});
       this.router.navigate(['/scrims']);
     })
